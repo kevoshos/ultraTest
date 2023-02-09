fcb0a9099957157496ebda0397b57757f7933433
Kevin Maldonado Alonzo <kevin@MacBook-Air-de-Kevin-2.local>
2023-02-09 17:33:07 -0600

    Created pom for Inventory and Cart shop pages

--- cypress/pages/inventory.page.cy.js
+++ cypress/pages/inventory.page.cy.js
@@ -3,6 +3,9 @@ class inventory{
     elements = {
 
         productsHeading: () => cy.get('.title').findByText("Products"),
+        addToCartButton: () => cy.get('.inventory_container').find('button', 'Add to cart'),
+        shoppingCartValue: () => cy.get('.shopping_cart_link').find("span[class='shopping_cart_badge']"),
+        shoppingCartLink: () => cy.get('.shopping_cart_link')
     
     }
 }
