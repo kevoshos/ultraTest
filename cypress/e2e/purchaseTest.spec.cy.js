1464f20a43986e9c344b25e3e9b4b75839e313eb
Kevin Maldonado Alonzo <kevin@MacBook-Air-de-Kevin-2.local>
2023-02-09 22:10:54 -0600

    Test implementation from the inventory flow to the checkout

--- cypress/e2e/purchaseTest.spec.cy.js
+++ cypress/e2e/purchaseTest.spec.cy.js
@@ -1,12 +1,16 @@
 import loginPage from "../pages/login.page.cy"
 import inventoryPage from "../pages/inventory.page.cy"
+import cartPage from "../pages/cart.page.cy"
+import customerInfoPage from "../pages/customerInfo.page.cy"
+import checkoutOverviewPage from "../pages/overview.page.cy"
 import logAccountFixture from "../fixtures/log-account.json"
+import customerInfoFixture from "../fixtures/customer-information.json"
 
-
-describe("End to end purchase with standard user", () =>{
+describe("End to end purchase with a standard user", () =>{
     const{ authorized_username, standard_password} = logAccountFixture
+    const{first_name, last_name, zip_code} = customerInfoFixture
 
-    beforeEach(()=>{
+    before(()=>{
         
         cy.visit('/')
         loginPage.elements.userNameInput().invoke('show').as('userName')
@@ -23,8 +27,7 @@ describe("End to end purchase with standard user", () =>{
             .should('exist')
             .click()
          cy.url()
-            should('eq', 'https://www.saucedemo.com/inventory.html')
-         inventoryPage.elements.productsHeading()
+            .should('eq', Cypress.config().baseUrl + '/inventory.html')
             .should('exist')
         
     })
@@ -32,7 +35,45 @@ describe("End to end purchase with standard user", () =>{
 
 it("Verify standard user is able to complete an end to end purchase", () =>{
 
-   
+    // Add all items available to the cart shop
+    let addButton = inventoryPage.elements.addToCartButton()
+    addButton.then(($ele) => { 
+    const itemCount = Cypress.$($ele).length;
 
-    })
-})
+        cy.get('.inventory_container').each(($button) => {
+
+            cy.wrap($button).find('button', 'Add to cart').click({multiple:true})
+
+            inventoryPage.elements.shoppingCartLink()
+                .should('contain',itemCount)
+                .click()
+
+           })  
+        })
+
+    cartPage.elements.cartHeading()
+            .should('exist')
+            .and('have.text', 'Your Cart')
+    cartPage.elements.checkoutButton()
+            .should('be.enabled')
+            .click()
+
+    // Add customer information
+    customerInfoPage.elements.infoHeader()
+        .should('exist')
+        .and('have.text', 'Checkout: Your Information')
+    customerInfoPage.fillCustomerInfo(first_name,last_name,zip_code)
+
+    // Overview and complete order
+    checkoutOverviewPage.elements.overviewHeader()
+        .should('exist')
+        .and('have.text','Checkout: Overview')
+    checkoutOverviewPage.elements.finishButton()
+        .should('be.enabled')
+        .click()
+    checkoutOverviewPage.elements.checkoutComplete()
+        .should('be.visible')
+
+  })
+
+    })    
